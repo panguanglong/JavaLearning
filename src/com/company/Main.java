be35c7c9b7d8033ae7630c96138820b0d215df25
panguanglong <pan_gl1992@hotmail.com>
2015-03-10 22:33:20 +0800

    建立第一个工程

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import com.company.oneday.HelloGit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        testOneDay();
+    }
+
+    public static void testOneDay() {
+        HelloGit.sayHello();
+    }
+}
---
+++ src/com/company/oneday/HelloGit.java
@@ -0,0 +1,12 @@
+package com.company.oneday;
+
+/**
+ * @author Administrator
+ * @time 2015/3/10
+ * @description:
+ */
+public class HelloGit {
+    public static void sayHello() {
+        System.out.println("hello, git");
+    }
+}
